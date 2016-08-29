b07fbea43cc846b2e21f29fa1f842bf39bd916f5
Cody W. Opel <codyopel@gmail.com>
2016-08-28 20:57:02 -0400

    refactor setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,36 @@
-import ez_setup
-ez_setup.use_setuptools()
+#!/usr/bin/env python3
 
-import setuptools
+from os import path as op
+from setuptools import setup
 
-setuptools.setup(
-    name="dale-beets-plugins",
-    version="0.1",
-    packages=setuptools.find_packages(),
-    install_requires=['beets>=1.2.2,==dev'],
+def _read(fname):
+    try:
+        return open(op.join(op.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+setup(
+    name='beets-moveall-artifacts',
+    version='0.0.0',
+    description='',
+    author='Cody Opel',
+    author_email='codyopel@gmail.com',
+    url='https://github.com/chlorm/beets-moveall-artifacts/',
+    license='MIT',
+    platforms='ALL',
+    long_description=_read('README.md'),
+    packages=[
+      'beetsplug',
+    ],
+    install_requires=[
+      'beets>=1.2.19',
+    ],
+    classifiers=[
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
