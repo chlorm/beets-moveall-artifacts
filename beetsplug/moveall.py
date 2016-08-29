41da4f91bf4c2443cb23ec1030222054a2dbf9d6
Cody W. Opel <codyopel@gmail.com>
2016-08-28 20:56:26 -0400

    add compyright header & use more explicit imports

--- beetsplug/moveall.py
+++ beetsplug/moveall.py
@@ -1,10 +1,29 @@
-import os.path
-import os
+# Copyright (c) 2013 Dale Sedivec
+# Copyright (c) 2016 Cody Opel <codyopel@gmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from beets import library, util
+from beets.plugins import BeetsPlugin
+from logging import getLogger
+from os import listdir, path
 import shutil
-import logging
-
-from beets import plugins, library, util
-
 
 log = logging.getLogger("beets")
 
